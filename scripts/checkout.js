d7a40fde9f7c000d4007c2b2fdcb8ebdbd73b918
raj patel <ajaykumar971142@gmail.com>
2025-12-24 22:13:58 +0530

    make delete button working

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,4 +1,4 @@
-import { cart, removeFromCart } from "../data/crat.js"
+import { cart, removeFromCart, updateCartqunatity } from "../data/crat.js"
 import { products } from '../data/products.js'
 
 
@@ -19,7 +19,7 @@ cart.forEach((item)=>{
     })
 
     cartSummary+=`
-    <div class="cart-item-container">
+    <div class="cart-item-container js-cart-item-container-${matchingproduct.id}">
             <div class="delivery-date">
               Delivery date: Tuesday, June 21
             </div>
@@ -106,6 +106,13 @@ document.querySelectorAll('.js-delete-link').forEach((link)=>{
         const productId=link.dataset.productId
         removeFromCart(productId)
         console.log(cart);
+
+        const container = document.querySelector(`.js-cart-item-container-${productId}`)
+        console.log(container)
+        container.remove();
+        
+
+        
     })
 })
 
